2a5998cd38c5e595174df4a41a0db68e16c377e1
Mohamed ellithy <Mohamedelliyhy6541@Gmail.com>
2025-11-24 14:06:21 +0200

    HANDEL SERVICES

--- DependancyInjection.cs
+++ DependancyInjection.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using SurveyBasket.Api.Mapping;
+using System.Reflection;
 
 namespace SurveyBasket.Api
 {
@@ -6,27 +7,37 @@ namespace SurveyBasket.Api
     {
         public static IServiceCollection AddDependancies(this IServiceCollection services) 
         {
-
             services.AddControllers();
             services.AddEndpointsApiExplorer();
-
-            /// add mapster 
-            TypeAdapterConfig mappingConfiguration = TypeAdapterConfig.GlobalSettings;
-            mappingConfiguration.Scan(Assembly.GetExecutingAssembly());
-            services.AddSingleton<IMapper>(new Mapper(mappingConfiguration));
+            services.AddFluentValidationsDependancies();
+            services.AddMappingDependancies();
+            services.AddservicesDependancies();
             
-            // add services 
-            services.AddSingleton<IPollService, PollService>();
-           
+            // add swagger()
+            services.AddSwaggerGen();
+            return services;
+        }
+        public static IServiceCollection AddFluentValidationsDependancies(this IServiceCollection services) 
+        {
             //add fluent validations 
             services.AddScoped<IValidator, CreatRequestValidator>();
             services.AddFluentValidationAutoValidation()
              .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            
-            // add swagger()
-            services.AddSwaggerGen();
-            
             return services;
+        }
+        public static IServiceCollection AddMappingDependancies(this IServiceCollection services) 
+        {
+            /// add mapster 
+             TypeAdapterConfig mappingConfiguration = TypeAdapterConfig.GlobalSettings;
+            mappingConfiguration.Scan(Assembly.GetExecutingAssembly());
+            services.AddSingleton<IMapper>(new Mapper(mappingConfiguration));
+            return services;
+        }
+        public static IServiceCollection AddservicesDependancies(this IServiceCollection services) 
+        {
+            // add services 
+            services.AddSingleton<IPollService, PollService>();
+           return services;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,9 +9,7 @@ namespace SurveyBasket
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            // Add services
-           
-           
+            
             /// add All services 
             builder.Services.AddDependancies();
 
