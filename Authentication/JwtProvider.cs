83f3ed16de7e9013099c295a51d19acfd34fee6a
mohamed Ellithy <139176314+mohamedellithy6541@users.noreply.github.com>
2026-01-05 01:49:01 +0200

    add comites and Formate

--- Authentication/JwtProvider.cs
+++ Authentication/JwtProvider.cs
@@ -10,6 +10,7 @@ namespace SurveyBasket.Api.Authentication
     {
         public (string token, int expiresIn) GenerateJwtToken(ApplicationUser user)
         {
+            // add clainms
             Claim[] claims = [
                 new(JwtRegisteredClaimNames.Sub,user.Id),
                 new(JwtRegisteredClaimNames.Email,user.Email!),
@@ -18,13 +19,14 @@ namespace SurveyBasket.Api.Authentication
                 new(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
             ];
 
+            //Key 
             var symmetricSecuirtiyKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("f844c97f267ceb6e94eed9afb8124e2e1c3f56ee294fcf5e7b8faa6849f3b707"));
+            //Credentials
             var singinngCredentials = new SigningCredentials(symmetricSecuirtiyKey, SecurityAlgorithms.HmacSha256);
-            
+            // ExpiresIN
             var expiresIn = 30;
-           
             var expiresInDate = DateTime.UtcNow.AddMinutes(expiresIn);
-           
+           // token 
             var token = new JwtSecurityToken(
                 issuer: "SurvayBasketApp",
                 audience: "SurvayBasketApp Users",
--- DependancyInjection.cs
+++ DependancyInjection.cs
@@ -85,8 +85,5 @@ namespace SurveyBasket.Api
             });
             return services;
         }
-
-
-
     }
 }
